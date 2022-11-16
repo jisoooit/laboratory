a5f2e02ce347b64dc4bc8a094eeee0281000fc21
jisoooit <ydsn336@naver.com>
2022-11-16 17:08:53 +0900

    feat: project setting

---
+++ jpa/src/main/java/com/example/bookmanager/controller/HelloWordController.java
@@ -0,0 +1,13 @@
+package com.example.bookmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "hello world";
+    }
+}
