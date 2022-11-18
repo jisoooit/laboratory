1f40a5e19769895a0901be704164296efdeed278
jisoooit <ydsn336@naver.com>
2022-11-18 17:50:31 +0900

    feat: java collection test

--- practice01/src/main/java/com/example/practice01/JavaTest.java
+++ practice01/src/main/java/com/example/practice01/JavaTest.java
@@ -1,23 +1,38 @@
 package com.example.practice01;
 
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 public class JavaTest {
-    private static long counter;
-
-    private static void wasCalled() {
-        counter++;
-    }
     public static void main(String[] args) {
+        MultiValueMap<String, Integer> multiValueMap = new LinkedMultiValueMap<>();
+        Map<String, Integer> map = new HashMap<>();
+        Map<String, List<Integer>> map2 = new HashMap<>();
+
+        multiValueMap.add("a",1);
+        multiValueMap.add("a",2);
+        multiValueMap.add("a",3);
+
+        map.put("a",1);
+        map.put("a",2);
+        map.put("a",3);
+
+        System.out.println(multiValueMap.get("a"));
+        System.out.println(map.get("a"));
 
 
-        List<String> list = Arrays.asList("abc1", "abc2","abc3");
-        counter = 0;
-        Stream<String> stream = list.stream().filter(element -> {
-            wasCalled();
-            return element.contains("2");
-        });
+        List<Integer> list = Arrays.asList(1, 2, null);
+        list.set(1, 10); // OK
+
+        List<Integer> list2 = List.of(1, 2, 3);
+
+        list.stream().forEach(System.out::println);
+        list2.stream().forEach(System.out::println);
     }
 }
