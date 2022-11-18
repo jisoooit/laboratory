6fb67c2cd9636ac3f39bfa9f5f4ef1cc3a99b082
jisoooit <ydsn336@naver.com>
2022-11-18 14:21:48 +0900

    feat : developer save

--- developer/src/main/java/com/example/developer/controller/DMakerController.java
+++ developer/src/main/java/com/example/developer/controller/DMakerController.java
@@ -1,5 +1,7 @@
 package com.example.developer.controller;
 
+import com.example.developer.service.DMakerService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +11,23 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class DMakerController {
-
+    private final DMakerService dMakerService;
     @GetMapping("/developers")
     public List<String> getAllDevelopers(){
         log.info("Get /developers Http/1.1");
 
         return Arrays.asList("snow", "grace","elsa");
+    }
+
+    @GetMapping("/create-developers")
+    public List<String> createDevelopers(){
+        log.info("Get /create-developers Http/1.1");
+
+        dMakerService.createDeveloper();
+
+        return List.of("elsa");
     }
 
 }
---
+++ developer/src/main/java/com/example/developer/service/DMakerService.java
@@ -0,0 +1,30 @@
+package com.example.developer.service;
+
+import com.example.developer.entity.Developer;
+import com.example.developer.repository.DeveloperRepository;
+import com.example.developer.type.DeveloperLevel;
+import com.example.developer.type.DeveloperSkillType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class DMakerService {
+    private final DeveloperRepository developerRepository;
+
+    @Transactional
+    public void createDeveloper(){
+        Developer developer = Developer.builder()
+                .developerLevel(DeveloperLevel.JUNIOR)
+                .developerSkillType(DeveloperSkillType.FRONT_END)
+                .experienceYears(2)
+                .name("elsa")
+                .age(10)
+                .build();
+
+        developerRepository.save(developer);
+    }
+}
+
