bb810c110fe4a53ecad0c37fa31252604ce8df6e
jisoooit <ydsn336@naver.com>
2022-11-27 14:07:32 +0900

    feat: ManyToMany 사용하지 않고 중간 테이블 만들기

--- jpa/src/test/java/com/example/bookmanager/repository/AuthorRepositoryTest.java
+++ jpa/src/test/java/com/example/bookmanager/repository/AuthorRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.bookmanager.repository;
 
 import com.example.bookmanager.domain.Author;
 import com.example.bookmanager.domain.Book;
+import com.example.bookmanager.domain.BookAndAuthor;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,8 @@ class AuthorRepositoryTest {
     private AuthorRepository authorRepository;
     @Autowired
     private BookRepository bookRepository;
+    @Autowired
+    private BookAndAuthorRepository bookAndAuthorRepository;
 
     @Test
     @Transactional
@@ -27,35 +30,61 @@ class AuthorRepositoryTest {
         Book book3 = givenBook("개발책1");
         Book book4 = givenBook("개발책2");
 
-        Author author1=givenAuthor("grace");
-        Author author2=givenAuthor("elsa");
+        Author author1 = givenAuthor("grace");
+        Author author2 = givenAuthor("elsa");
 
-        book1.addAuthor(author1);
-        book2.addAuthor(author2);
-        book3.addAuthor(author1, author2);
-        book4.addAuthor(author1, author2);
+//        book1.addAuthor(author1);
+//        book2.addAuthor(author2);
+//        book3.addAuthor(author1, author2);
+//        book4.addAuthor(author1, author2);
+//
+//        author1.addBook(book1, book3, book4);
+//        author2.addBook(book2, book3, book4);
 
-        author1.addBook(book1, book3, book4);
-        author2.addBook(book2, book3, book4);
+        BookAndAuthor bookAndAuthor1 = givenBookAndAuthor(book1, author1);
+        BookAndAuthor bookAndAuthor2 = givenBookAndAuthor(book2, author2);
+        BookAndAuthor bookAndAuthor3 = givenBookAndAuthor(book3, author1);
+        BookAndAuthor bookAndAuthor4 = givenBookAndAuthor(book3, author2);
+        BookAndAuthor bookAndAuthor5 = givenBookAndAuthor(book4, author1);
+        BookAndAuthor bookAndAuthor6 = givenBookAndAuthor(book4, author2);
+
+        book1.addBookAndAuthors(bookAndAuthor1);
+        book2.addBookAndAuthors(bookAndAuthor2);
+        book3.addBookAndAuthors(bookAndAuthor3, bookAndAuthor4);
+        book4.addBookAndAuthors(bookAndAuthor5, bookAndAuthor6);
+
+        author1.addBookAndAuthors(bookAndAuthor1, bookAndAuthor3, bookAndAuthor5);
+        author2.addBookAndAuthors(bookAndAuthor2, bookAndAuthor4, bookAndAuthor6);
 
         bookRepository.saveAll(Lists.newArrayList(book1, book2, book3, book4));
         authorRepository.saveAll(Lists.newArrayList(author1, author2));
 
-        System.out.println("authors through book: "+ bookRepository.findAll().get(2).getAuthors());
-        System.out.println("books through author: "+authorRepository.findAll().get(0).getBooks());
+//        System.out.println("authors through book: " + bookRepository.findAll().get(2).getAuthors());
+//        System.out.println("books through author: " + authorRepository.findAll().get(0).getBooks());
+
+        bookRepository.findAll().get(2).getBookAndAuthors().forEach(o -> System.out.println(o.getAuthor())); //여기서 o가 BookAndAuthor
+        authorRepository.findAll().get(0).getBookAndAuthors().forEach(o -> System.out.println(o.getBook()));
     }
 
-    private Book givenBook (String name) {
+    private Book givenBook(String name) {
         Book book = new Book();
         book.setName(name);
         return bookRepository.save(book);
     }
 
-    private Author givenAuthor(String name){
+    private Author givenAuthor(String name) {
         Author author = new Author();
         author.setName(name);
 
         return authorRepository.save(author);
     }
 
+    private BookAndAuthor givenBookAndAuthor(Book book, Author author) {
+        BookAndAuthor bookAndAuthor = new BookAndAuthor();
+        bookAndAuthor.setBook(book);
+        bookAndAuthor.setAuthor(author);
+
+        return bookAndAuthorRepository.save(bookAndAuthor);
+
+    }
 }
